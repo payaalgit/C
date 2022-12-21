d4eadde5d2e3220d38a53972657b354041cf0a8b
payaalgit <120472273+payaalgit@users.noreply.github.com>
2022-12-21 09:20:34 +0530

    Add files via upload

---
+++ 1_to_n_even_Odd.c
@@ -0,0 +1,31 @@
+//This program is about to separate and print the odd and even numbers up to given limit using For Loop
+
+   
+#include<stdio.h>
+int main()
+{
+  int i,n,even=0,odd=0;
+  printf("\nEnter the Ending value:");
+  scanf("%d",&n);
+  printf("\nEven numbers:");
+  for(i=0;i<=n;i++)
+  {
+    if(i%2==0)
+    {
+      printf("\n%d",i);
+      even++;
+    }
+  }
+  printf("\nOdd numbers:");
+  for(i=1;i<=n;i++)
+  {
+    if(i%2==1)
+    {
+      printf("\n%d",i);
+      odd++;
+    }
+  }
+  printf("\nTotal even numbers:%d",even);
+  printf("\nTotal odd numbers:%d",odd);
+}
+
