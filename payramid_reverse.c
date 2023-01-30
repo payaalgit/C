958b0e706acfd5585281f320eb89176d256f0573
payaalgit <120472273+payaalgit@users.noreply.github.com>
2023-01-30 18:08:06 +0530

    Add files via upload

---
+++ payramid_reverse.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   int i,j,row;
+   printf("enter the number of row");
+   scanf("%d",& row);
+   for (i=row;i>=1;--i)
+   {
+   	for(j=1;j<=i;++j)
+   	printf("*");
+   	printf("\n");
+   	
+   }
+}
